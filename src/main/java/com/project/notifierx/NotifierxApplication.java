d46e9a750186dd8ac4dae4c8bc428a644a7ef176
Yazan Homssi <homssi002@gmail.com>
2026-07-17 16:21:11 +0800

    chore: bootstrap Spring Boot structure with core dependencies

---
+++ src/main/java/com/project/notifierx/NotifierxApplication.java
@@ -0,0 +1,13 @@
+package com.project.notifierx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotifierxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotifierxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/notifierx/NotifierxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.notifierx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotifierxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
